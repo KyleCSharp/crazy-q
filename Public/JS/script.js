25db8220d84b9a3b0d8365d77a84db93f9442c68
Kyle Rigler <kylelrigler@gmail.com>
2023-07-13 19:55:40 -0400

    added real comics

--- Public/JS/script.js
+++ Public/JS/script.js
@@ -84,4 +84,19 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
-  
+  $(document).ready(function() {
+    var currentImageIndex = 0;
+    var images = $(".comics-div img");
+    var numImages = images.length;
+
+    function showNextImage() {
+      images.hide(); // Hide all images
+      images.eq(currentImageIndex).show(); // Show the current image
+
+      currentImageIndex = (currentImageIndex + 1) % numImages; // Update the current image index
+
+      setTimeout(showNextImage, 2000); // Change image every 2 seconds (adjust this value as needed)
+    }
+
+    showNextImage(); // Start the slideshow
+  });
